895c4cc68f2886197e4ddf7b7f4d46ec00dcc8c0
meetkavad <meet.kavad07@gmail.com>
2024-03-05 13:43:38 +0530

    firt commit , backend setup

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const connectDB = require("./Database/connect");
+const authRouter = require("./Router/authRouter");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/Weatlh-View/v1/auth", authRouter);
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log("db connected");
+    app.listen(port, console.log(`server listening at port ${port}`));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
