e3a023eb12026a3967d77d6eb15072501dff0fb0
meetkavad <meet.kavad07@gmail.com>
2024-04-27 09:20:47 +0530

    integration for signup and login

--- client/src/App.js
+++ client/src/App.js
@@ -10,50 +10,50 @@ import InvestmentInfo from "./components/InvestmentInfo";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { colors } from "./values/colors";
 import AssetsLiabilityInfo from "./components/AssetsLiabilityInfo";
-import Dashboard from "./components/Dashboard";
+import Dashboard from "./components/DashBoard";
 
 function App() {
-    return (
-        <div style={{ backgroundColor: colors.white }}>
-            <BrowserRouter>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <>
-                                <Navigation />
-                                <LandingPage />
-                            </>
-                        }
-                    />
-                    <Route
-                        path="/dashboard"
-                        element={
-                            <>
-                                <Navigation />
-                                <Dashboard />
-                            </>
-                        }   
-                    />
-                    <Route path="/personal_info" element={<PersonalInfo />} />
-                    <Route path="/about_us" element={<AboutUs />} />
-                    <Route path="/login_signup" element={<LoginSignup />} />
-                    <Route
-                        path="/personal_info/financial_info"
-                        element={<FinancialInfo />}
-                    />
-                    <Route
-                        path="/Personal_info/financial_info/AssetsLiability_info"
-                        element={<AssetsLiabilityInfo />}
-                    />
-                    <Route
-                        path="/Personal_info/financial_info/AssetsLiability_info/Investment_info"
-                        element={<InvestmentInfo />}
-                    />
-                </Routes>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div style={{ backgroundColor: colors.white }}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Navigation />
+                <LandingPage />
+              </>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <>
+                <Navigation />
+                <Dashboard />
+              </>
+            }
+          />
+          <Route path="/personal_info" element={<PersonalInfo />} />
+          <Route path="/about_us" element={<AboutUs />} />
+          <Route path="/login_signup" element={<LoginSignup />} />
+          <Route
+            path="/personal_info/financial_info"
+            element={<FinancialInfo />}
+          />
+          <Route
+            path="/Personal_info/financial_info/AssetsLiability_info"
+            element={<AssetsLiabilityInfo />}
+          />
+          <Route
+            path="/Personal_info/financial_info/AssetsLiability_info/Investment_info"
+            element={<InvestmentInfo />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
